fa50665ed3d8893194aa2e0f31b195b270b50c38
Ravi Tiwari <rtiwariops@gmail.com>
2023-02-06 18:16:59 -0700

    updated event, post and comments logic

--- comments/index.js
+++ comments/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -23,9 +24,23 @@ app.post("/posts/:id/comments", (req, res) => {
 
   comments.push({ id: commentId, content });
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postID: req.params.id,
+    },
+  });
+
   commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
