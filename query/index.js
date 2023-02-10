f5ed9c6ec7a0a1f2e30171de0dd9e5cb40e39c86
Ravi Tiwari <rtiwariops@gmail.com>
2023-02-09 23:14:47 -0700

    New query service added

---
+++ query/index.js
@@ -0,0 +1,16 @@
+/** @format */
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {});
+
+app.get("/events", (req, res) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
